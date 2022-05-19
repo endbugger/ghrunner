351648cda44396a8a930e3eec0b60afdeb9f935f
endbugger <islammominul.ihemal@gmail.com>
2022-05-19 23:43:59 +0000

    ghrunner: GITHUB_TOKEN -> GH_TOKEN

--- push.sh
+++ push.sh
@@ -8,9 +8,9 @@ git fetch micode laurel-r-oss
 git checkout -b laurel-r-oss micode/laurel-r-oss
 
 curl \
-https://api.github.com/user/repos\?access_token=${GITHUB_TOKEN} \
+https://api.github.com/user/repos\?access_token=${GH_TOKEN} \
 -d '{"name": "'"android_kernel_xiaomi_laurel_sprout"'","description": "'"laurel_sprout(Mi A3) R kernel source"'","private": false,"has_issues": true,"has_projects": false,"has_wiki": true}'
 
-git push https://"${GITHUB_TOKEN}"@github.com/endbugger/android_kernel_xiaomi_laurel_sprout.git "laurel-r-oss" --force
+git push https://"${GH_TOKEN}"@github.com/endbugger/android_kernel_xiaomi_laurel_sprout.git "laurel-r-oss" --force
 
 
